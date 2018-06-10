efdd85ea23b9e87bf1de47401f7aae6cd9dfe35d
msimonin <matthieu.simonin@inria.fr>
2018-06-10 22:01:05 +0200

    initial

---
+++ enos_kubernetes/tests/unit/test_enos_kubernetes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `enos_kubernetes` package."""
+
+import unittest
+
+
+class TestEnos_kubernetes(unittest.TestCase):
+    """Tests for `enos_kubernetes` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
